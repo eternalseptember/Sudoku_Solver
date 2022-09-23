2e4d99166173aefad42cd2e490e27b7d6080b217
KQ <eternal.september@gmail.com>
2022-09-23 13:38:54 -0400

    adding test for new intersection functions

--- strats/README.md
+++ strats/README.md
@@ -1,1 +1,3 @@
+# Info
+
 All of these functions are imported by the main sudoku_solver class.
---
+++ tests/test_intersection_line.py
@@ -0,0 +1,36 @@
+"""
+Col test:
+3 is confined to (rows 6-8; col 0).
+Remove 3 as possibilities from (7,1) and (8,2).
+
+6 is confined to (rows 3-5 ; col 1).
+Remove 6 as possibilities from (rows 3-5; col 2).
+"""
+
+
+import sys
+sys.path.append('../')
+from sudoku_solver import *
+
+
+sudoku = Sudoku_Solver()
+sudoku.import_board("test_boards/ints_line_col.txt")
+sudoku.print_board()
+print('===============================')
+
+
+# Testing steps
+print('Init reduce:')
+sudoku.solve_queue()
+sudoku.print_board()
+# sudoku.print_possible_values()
+print('===============================')
+
+
+print('Check line intersections:')
+sudoku.check_intersection_lines()
+sudoku.print_board()
+sudoku.print_possible_values()
+print('===============================')
+
+
