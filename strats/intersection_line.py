da7f3fa2bfdd416685e0a0d2572c3b8458a55808
KQ <eternal.september@gmail.com>
2022-09-28 15:16:35 -0400

    filling out functions

--- strats/intersection_line.py
+++ strats/intersection_line.py
@@ -4,13 +4,15 @@ def check_intersection_lines(self):
     Then check if all possible locations for a missing value is in the same box.
     If it is, then remove it from the rest of the box.
     """
-    print()
-
+    # self.check_intersection_rows()
+    self.check_intersection_cols()
 
 
 def check_intersection_rows(self):
     """
     Tally up all possible values in each row.
+    Then check if all possible locations for a missing value is in the same box.
+    If it is, then remove it from the rest of the box.
     """
     for row_step in range(9):
         row_missing_vals = {}
@@ -25,7 +27,21 @@ def check_intersection_rows(self):
 
 
 def check_intersection_cols(self):
-    return None
+    """
+    Tally up all possible values in each col.
+    Then check if all possible locations for a missing value is in the same box.
+    If it is, then remove it from the rest of the box.
+    """
+    for col_step in range(9):
+        col_missing_vals = {}
+
+        # Collect list of missing values and their locations in this col.
+        for row_step in range(9):
+            this_cell = (row_step, col_step)
+            self.set_lookup_table(this_cell, col_missing_vals)
+
+
+
 
 
 def check_intersection_row(self, coord):
