1a678a52772dd4a8840b0ead1054644b311e5489
KQ <eternal.september@gmail.com>
2023-01-02 15:26:08 -0500

    filllng out functions

--- strats/swordfish.py
+++ strats/swordfish.py
@@ -244,6 +244,7 @@ def two_search(self, poss_val, row_list, cols_list):
     # in row 7, 9 is a valid candidate only in (2, 7) and (8, 7).
     # then check the third spot.
     intersection_1 = self.intersection_of_two(col_list_0, col_list_1)
+    intersection_2 = self.intersection_of_two(col_list_0, col_list_2)
 
 
 
