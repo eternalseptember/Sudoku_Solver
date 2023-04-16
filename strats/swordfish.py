d814044b55e09004c3fa1a822a90eeb8c59d6943
KQ <eternal.september@gmail.com>
2023-04-16 13:20:59 -0400

    filling out function

--- strats/swordfish.py
+++ strats/swordfish.py
@@ -328,7 +328,8 @@ def sf_check_loop(self, poss_val, row_list, ints_list):
             if (col_1_in_row_2 is True) and (col_1_in_row_3 is False):
                 # check if col_2 and look for the third coord
                 if (col_2_in_row_2 is False) and (col_2_in_row_3 is True):
-                    print()
+                    sf_cols_1_reduced = sf_cols_1.copy()
+                    
 
             elif (col_1_in_row_2 is False) and (col_1_in_row_3 is True):
                 # check col_2
